c7552ad958d243b54f490d47dcdfcfd8a07a690d
Icy <204663820+icyvert@users.noreply.github.com>
2025-12-15 23:25:19 +0530

    remove context menu and fix replies

--- cogs/chat.py
+++ cogs/chat.py
@@ -6,12 +6,6 @@ from discord.ext.commands import Context
 class Chat(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-    
-    @app_commands.context_menu(name="the what")
-    @app_commands.allowed_installs(guilds=True, users=True)
-    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-    async def the_what(self, interaction: discord.Interaction, message: discord.Message) -> None:
-        await interaction.response.send_message("the what")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
@@ -19,6 +13,8 @@ class Chat(commands.Cog):
             context: Context = await self.bot.get_context(message)
             if not context.valid and context.prefix:
                 await message.reply("what")
+                return
+            return
 
 async def setup(bot) -> None:
     await bot.add_cog(Chat(bot))
