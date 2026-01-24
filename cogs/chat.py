550d75eb33ebe7a7a65cc801010455753fd8e1ea
Icy <204663820+icyvert@users.noreply.github.com>
2026-01-24 19:05:01 +0530

    reddit embed test

--- cogs/chat.py
+++ cogs/chat.py
@@ -1,4 +1,5 @@
 import discord
+import re
 
 from discord.ext import commands
 from discord.ext.commands import Context
@@ -7,15 +8,30 @@ from discord.ext.commands import Context
 class Chat(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.domains = {"reddit.com": "rxddit.com"}
+        regex = "|".join([re.escape(d) for d in self.domains.keys()])
+        self.urls = re.compile(rf"https://(?:www\.)?({regex})/[^\s]+")
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot:
             return
+
         if self.bot.user in message.mentions:
             context: Context = await self.bot.get_context(message)
             if context.prefix and not context.valid:
                 await message.reply("what")
+
+        match = self.urls.search(message.content)
+        if match:
+            url = match.group(0)
+            domain = match.group(1)
+
+            for domain in self.domains:
+                new_domain = self.domains[domain]
+                new_url = url.replace(domain, new_domain)
+                await message.edit(suppress=True)
+                await message.channel.send(new_url)
 
 
 async def setup(bot: commands.Bot) -> None:
