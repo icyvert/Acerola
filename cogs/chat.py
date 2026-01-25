1b6f41f3317d1864c67be68c6b136d580073d4b0
Icy <204663820+icyvert@users.noreply.github.com>
2026-01-25 16:47:39 +0530

    webhook cache

--- cogs/chat.py
+++ cogs/chat.py
@@ -18,25 +18,25 @@ class Chat(commands.Cog):
             "twitter.com": "fxtwitter.com",
         }
         regex = "|".join([re.escape(d) for d in self.domains.keys()])
-        self.urls = re.compile(rf"https://(?:www\.)?({regex})/[^\s]+")
+        self.urls = re.compile(rf"https://(?:www\.)?({regex})/\S+")
+        self.webhook_cache = {}
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
         if message.author.bot:
             return
 
-        if self.bot.user in message.mentions:
-            context: Context = await self.bot.get_context(message)
-            if context.prefix and not context.valid:
-                await message.reply("what")
-
         match = self.urls.search(message.content)
         if match:
             try:
-                webhooks = await message.channel.webhooks()
-                webhook = discord.utils.get(webhooks, name="Acerola")
-                if webhook is None:
-                    webhook = await message.channel.create_webhook(name="Acerola")
+                if message.channel.id in self.webhook_cache:
+                    webhook = self.webhook_cache[message.channel.id]
+                else:
+                    webhooks = await message.channel.webhooks()
+                    webhook = discord.utils.get(webhooks, name="Acerola")
+                    if webhook is None:
+                        webhook = await message.channel.create_webhook(name="Acerola")
+                    self.webhook_cache[message.channel.id] = webhook
             except Exception:
                 logger.exception("Webhook failed")
                 return
@@ -52,8 +52,16 @@ class Chat(commands.Cog):
                     avatar_url=message.author.display_avatar.url,
                 )
                 await message.delete()
+            except discord.NotFound:
+                self.webhook_cache.pop(message.channel.id, None)
             except Exception:
                 logger.exception("Embed failed")
+            return
+
+        if self.bot.user in message.mentions:
+            context: Context = await self.bot.get_context(message)
+            if context.prefix and not context.valid:
+                await message.reply("what")
 
 
 async def setup(bot: commands.Bot) -> None:
